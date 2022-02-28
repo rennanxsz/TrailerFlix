463ec31735dd488530272372c9ff1ce8be65bb47
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-02-27 22:05:27 -0300

    Creating a new project and put images
    Criando mais um projeto do Eric e já adicionando as imagens que serão usadas.

---
+++ TrailerFlix/TrailerFlix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrailerFlix
+//
+//  Created by Rennan Bruno on 27/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
