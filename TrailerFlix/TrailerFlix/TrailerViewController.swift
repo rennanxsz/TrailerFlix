aeda2c23dd40b1f1ddf32b3f3d84e55bff50dc3e
rennanxsz <43283810+rennanxsz@users.noreply.github.com>
2022-02-28 00:03:33 -0300

    Completed layout
    Layout das telas iniciais completo.

---
+++ TrailerFlix/TrailerFlix/TrailerViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TrailerViewController.swift
+//  TrailerFlix
+//
+//  Created by Rennan Bruno on 27/02/22.
+//
+
+import UIKit
+
+class TrailerViewController: UIViewController {
+    
+    
+    @IBOutlet weak var ivTrailer: UIImageView!
+    @IBOutlet weak var lbTitle: UILabel!
+    @IBOutlet weak var lbYear: UILabel!
+    @IBOutlet weak var lbRating: UILabel!
+    @IBOutlet weak var viTrailer: UIView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func close(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
--- TrailerFlix/TrailerFlix/ViewController.swift
+++ TrailerFlix/TrailerFlix/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func watchRandomTrailer(_ sender: UIButton) {
+    }
+    
 }
 
